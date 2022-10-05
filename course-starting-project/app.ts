c725be124cb352432d9b6d9e8f8d0a1fc45d0687
Adriana <adrianaalves098@gmail.com>
2022-10-05 23:13:41 +0100

    l19 Tuples

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,5 +1,20 @@
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Consuela",
   age: 31,
-  hobbies: [],
+  hobbies: ["Gaming", "TvShows"],
+  role: [2, "edit"],
 };
+
+let favoriteHobbies: string[];
+favoriteHobbies = ["Sports"];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+}
