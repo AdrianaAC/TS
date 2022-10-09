cab7030d9923be332dd0a80a4e03bd18ba9f04a7
Adriana <adrianaalves098@gmail.com>
2022-10-09 22:08:43 +0100

    init command

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,3 +1,4 @@
 let userName = "Consuela";
 console.log(userName);
 //--watch or -w to get watch mode on one file
+//tsc --init to start a project and watch the whole project(everything in the folder where the command runs is a project and should be managed by ts). Only required once
