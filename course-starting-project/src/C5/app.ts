c1bbad363c13400934098847535510fda15e30f9
Adriana <adrianaalves098@gmail.com>
2022-10-11 23:16:58 +0100

    l67 getters & setters

--- course-starting-project/src/C5/app.ts
+++ course-starting-project/src/C5/app.ts
@@ -1,13 +1,16 @@
 class Department {
-  name: string;
-  employees: string[] = [];
+  //   public name: string;
+  //   private readonly id: number;
+  //   protected makes it acessible from extended instances of class, private blocks that access
+  protected employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private readonly id: number, public name: string) {
+    this.name = name;
+    this.id = id;
   }
 
   describe(this: Department) {
-    console.log("Department:" + this.name);
+    console.log("Department:" + this.name + " id:" + this.id);
   }
 
   addEmployee(employee: string) {
@@ -15,14 +18,42 @@ class Department {
   }
 
   prtEmployeeInfo() {
-    console.log(this.employees.length);
+    console.log(this.name + " employers nº " + this.employees.length);
     console.log(this.employees);
   }
 }
 
-const it = new Department("IT");
-it.describe();
-it.addEmployee("Consuela");
-it.addEmployee("Max");
-it.employees[2] = "ANNA";
-it.prtEmployeeInfo();
+class ITDpt extends Department {
+  constructor(id: number, public admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+}
+
+class AccDpt extends Department {
+  constructor(id: number, private reports: number[], public admins: string[]) {
+    super(id, "Acc");
+    this.admins = admins;
+    this.reports = reports;
+  }
+  addReport(reportID: number, reportName: string) {
+    this.reports.push(reportID);
+  }
+}
+
+const itDpt = new ITDpt(1, ["Consuela"]);
+const accDpt = new AccDpt(2, [0, 256, 999], ["Luke"]);
+
+//itDpt.describe();
+itDpt.addEmployee("John");
+itDpt.addEmployee("Jane");
+accDpt.addEmployee("otherJohn");
+accDpt.addEmployee("otherJane");
+accDpt.prtEmployeeInfo();
+itDpt.prtEmployeeInfo();
+
+//const it = new Department(1, "IT");
+//it.employees[2] = "ANNA"; --> can't access because of private modifier on field employees
+
+console.log(itDpt);
+console.log(accDpt);
