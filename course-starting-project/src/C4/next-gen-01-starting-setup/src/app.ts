3f1ee16c5db5a978eb787f0ca7fe87a4588a510d
Adriana <adrianaalves098@gmail.com>
2022-10-11 02:10:00 +0100

    c4 done

---
+++ course-starting-project/src/C4/next-gen-01-starting-setup/src/app.ts
@@ -0,0 +1,10 @@
+// https://kangax.github.io/compat-table/es6/  --> list of js features and what brownser and compilers supports it
+
+const userName = "Consuela";
+let age = 31;
+
+const add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+console.log(add(2.7, 89));
