ba25ac70bcf2ca18a4ef3491f67df5fdf0491f1a
Adriana <adrianaalves098@gmail.com>
2022-10-12 01:59:14 +0100

    abstract classes starting

--- course-starting-project/src/C5/app.ts
+++ course-starting-project/src/C5/app.ts
@@ -31,29 +31,46 @@ class ITDpt extends Department {
 }
 
 class AccDpt extends Department {
-  constructor(id: number, private reports: number[], public admins: string[]) {
+  private lastReport: [number, string];
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("no report found");
+  }
+
+  constructor(
+    id: number,
+    private report: [[number, string]],
+    public admins: string[]
+  ) {
     super(id, "Acc");
     this.admins = admins;
-    this.reports = reports;
+    this.lastReport = report[0];
   }
-  addReport(reportID: number, reportName: string) {
-    this.reports.push(reportID);
+
+  addReport(reportID: number, reportMessage: string) {
+    this.report.push([reportID, reportMessage]);
+    this.lastReport = [reportID, reportMessage];
   }
 }
 
 const itDpt = new ITDpt(1, ["Consuela"]);
-const accDpt = new AccDpt(2, [0, 256, 999], ["Luke"]);
+const accDpt = new AccDpt(2, [[1, "Firtst Report"]], ["Luke"]);
 
 //itDpt.describe();
 itDpt.addEmployee("John");
 itDpt.addEmployee("Jane");
 accDpt.addEmployee("otherJohn");
 accDpt.addEmployee("otherJane");
-accDpt.prtEmployeeInfo();
-itDpt.prtEmployeeInfo();
+//accDpt.prtEmployeeInfo();
+//itDpt.prtEmployeeInfo();
 
 //const it = new Department(1, "IT");
 //it.employees[2] = "ANNA"; --> can't access because of private modifier on field employees
+
+accDpt.addReport(2, "other report");
 
 console.log(itDpt);
 console.log(accDpt);
