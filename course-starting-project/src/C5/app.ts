99ea6748bac8d11f784896fada16b97103d482d8
Adriana <adrianaalves098@gmail.com>
2022-10-13 00:12:32 +0100

    Starting Chapter6

--- course-starting-project/src/C5/app.ts
+++ course-starting-project/src/C5/app.ts
@@ -1,7 +1,7 @@
 interface Person {
   name: string;
   age: number;
-  likes: string[];
+  likes?: string[];
 
   greet(phrase: string): void;
 }
@@ -11,10 +11,21 @@ let p1: Person;
 p1 = {
   name: "Consuela",
   age: 31,
-  likes: ["cars", "trains"],
   greet(phrase: string) {
     console.log(phrase + this.name + "and i'm " + this.age);
   },
 };
 
 p1.greet("Hi, i am ");
+
+// type AddFn = (a: number, b: number) => number;
+
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add2: AddFn;
+
+add2 = (n1: number, n2: number) => {
+  return n1 + n2;
+};
