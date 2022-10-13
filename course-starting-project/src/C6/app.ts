f3c970f7024919c23420ddfba931387fb25cbd9f
Adriana <adrianaalves098@gmail.com>
2022-10-13 01:25:46 +0100

    typeguards

--- course-starting-project/src/C6/app.ts
+++ course-starting-project/src/C6/app.ts
@@ -20,3 +20,27 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+//Type guard using typeOf
+function add3(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function prtEmployeeInfo(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+
+  if ("priveleges" in emp) {
+    console.log("Admin Privileges: " + emp.priveleges);
+  }
+
+  if ("startDate" in emp) {
+    console.log("Employee start date: " + emp.startDate);
+  }
+}
+
+prtEmployeeInfo(e1);
