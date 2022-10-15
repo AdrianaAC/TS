e151da7705f887dc1ca540edf3ad8a1af515d439
Adriana <adrianaalves098@gmail.com>
2022-10-16 00:21:04 +0100

    more on generic functions

--- course-starting-project/src/C7/app.ts
+++ course-starting-project/src/C7/app.ts
@@ -1,7 +1,24 @@
 const names: Array<string> = ["Max", "Manuel"]; //== string[]. Array is a generic type and needs a value between <>
 
 function merge<T extends object, U>(objA: T, objB: U) {
+  //constraints in generics
   return Object.assign(objA, objB);
 }
 
 const mergedObj = merge({ name: "Consuela" }, { age: 30 });
+console.log(mergedObj);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDesc<T extends Lengthy>(element: T) {
+  let descText = "Got no value!";
+
+  if (element.length > 0) {
+    descText = "Got " + element.length + " elements";
+  }
+  return [element, descText];
+}
+
+console - log(countAndDesc("Hello there!"));
